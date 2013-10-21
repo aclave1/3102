e8de83f84c8577c67630c32e10240fbf7b7132bc
Alex Clavelle <aclav3113@gmail.com>
2013-10-21 12:45:19 -0500

    added a method to parse a text file.

--- 1prob/SortComp.java
+++ 1prob/SortComp.java
@@ -4,9 +4,32 @@ Alex Clavelle
 Kelsey Cameron
 
 */
+import java.util.LinkedList;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.*;
 class SortComp{
 	public static void main(String[] args){
-	System.out.println("hey");
+		Path path = Paths.get(args[0]);
+		LinkedList file = parseFile(path);		
+
 		
+	}
+
+	public static LinkedList<String> parseFile(Path path){
+		LinkedList<String> lines = new LinkedList<String>();
+		Charset charset = Charset.forName("US-ASCII");
+		try(BufferedReader reader = Files.newBufferedReader(path,charset)){
+			String line = null;
+			while((line = reader.readLine()) != null){
+				lines.add(line);
+				System.out.println(line);
+			}
+		}
+		catch (IOException x){
+			System.err.format("IOException: %s%n",x);
+		}
+		return lines;
 	}
 }
