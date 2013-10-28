8a70093731613a807808f5a041abd86f1beba425
Alex Clavelle <aclav3113@gmail.com>
2013-10-28 15:48:26 -0500

    Skeleton code for the K-ary tree

---
+++ 2prob/KAry.java
@@ -0,0 +1,10 @@
+/**
+@authors: Alex Clavelle, Kelsey Cameron
+Description: Creates a K-ary heap where K is the maximum number of children for a non-leaf node.
+*/
+
+public class KAry{
+	public static void main(String[] args){
+		
+	}
+}
